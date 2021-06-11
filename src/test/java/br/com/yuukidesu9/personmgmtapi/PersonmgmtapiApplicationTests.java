1681b6130ccc7aa767366faff4364e06e0fd01a5
Yuuki the Handyfox (João Costa) <joaoinstaller@gmail.com>
2021-06-11 11:03:27 -0400

    First commit

---
+++ src/test/java/br/com/yuukidesu9/personmgmtapi/PersonmgmtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yuukidesu9.personmgmtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonmgmtapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
